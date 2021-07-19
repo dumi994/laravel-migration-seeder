5d48712c9822d09a4b8c351873c3299834fd57ac
dumi994 <dumi.9944@gmail.com>
2021-07-19 18:04:29 +0200

    collegato sass e create le cardc

--- blog/resources/views/welcome.blade.php
+++ blog/resources/views/welcome.blade.php
@@ -4,26 +4,31 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>Laravel_Seeders</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
 
         <!-- Styles -->
+        <link rel="stylesheet" href="../public/css/app.css">
     </head>
     <body>
-        <div class="flex-center position-ref full-height">
+        <header class="header">
+            <h1>Prenota La Tua Vacanza</h1>
+        </header>
+        <div class="container">
           @foreach($flights as $flight)
           <div class="card">
-                <h2>Fantastica vacanza in {{$flight->destinazione}}</h2>
+              <a href="">
                 <img src="{{$flight->poster}}" alt="">
+                <h2>Fantastica vacanza in {{$flight->destinazione}}</h2>
                 <p>Della durata di: {{$flight->durata}} giorni</p>
                 <p>Partenza da: {{$flight->volo}}</p>
                 <p>Servizi inclusi: {{$flight->servizi}}</p>
                 <p>Pasti inclusi: {{$flight->pasti}}</p>
                 <p>Prezzo: {{$flight->prezzo}} &euro;</p>
-          </div>
-                
+              </a> 
+          </div>   
           @endforeach
         </div>
     </body>
